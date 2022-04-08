94fea91796bca60651ce78fc49e8c8e155c2baa2
ybt01 <86238013+ybt01@users.noreply.github.com>
2022-04-08 17:24:27 +0800

    Update sw.js

--- cache/sw.js
+++ cache/sw.js
@@ -3,7 +3,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'jquery.min.js'
+  'https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
