9b12162035bbe650411bbaa06e40060cc290cd50
ybt01 <86238013+ybt01@users.noreply.github.com>
2023-05-18 16:45:17 +0800

    Update demo.js

--- upload/demo.js
+++ upload/demo.js
@@ -14,93 +14,34 @@ function closeModal() {  //关闭上传框
 document.getElementsByClassName('overlay')[0].addEventListener('click', closeModal, false);
 document.getElementById('close').addEventListener('click', closeModal, false);
 
-//利用html5 FormData() API,创建一个接收文件的对象，因为可以多次拖拽，这里采用单例模式创建对象Dragfiles
-var Dragfiles=(function (){
-	var instance;
-	return function(){
-		if(!instance){
-			instance = new FormData();
-		}
-		return instance;
-	}
-}());
-//为Dragfiles添加一个清空所有文件的方法
-FormData.prototype.deleteAll=function () {
-	var _this=this;
-	this.forEach(function(value,key){
-		_this.delete(key);
-	})
-}
-
-// //添加拖拽事件
-// var dz = document.getElementById('content');
-// dz.ondragover = function (ev) {
-// 	//阻止浏览器默认打开文件的操作
-// 	ev.preventDefault();
-// 	//拖入文件后边框颜色变红
-// 	this.style.borderColor = 'red';
-// }
-
-// dz.ondragleave = function () {
-// 	//恢复边框颜色
-// 	this.style.borderColor = 'gray';
-// }
-// dz.ondrop = function (ev) {
-// 	//恢复边框颜色
-// 	this.style.borderColor = 'gray';
-// 	ev.preventDefault();
-// 	}
 
 
 function blink()
 {
   document.getElementById('content').style.borderColor = 'gray';
 }
-//ajax上传文件
-function upload(){
-	if(document.getElementsByTagName('tbody')[0].hasChildNodes()==false){
-		document.getElementById('content').style.borderColor = 'red';
-		setTimeout(blink,200);
-		return false;
-	}
-	var data=Dragfiles(); //获取formData
-	$.ajax({
-		url: 'upload',
-		type: 'POST',
-		data: data,
-		async: true,
-		cache: false,
-		contentType: false,
-		processData: false,
-		success: function (data) {
-			alert('succeed!')  //可以替换为自己的方法
-			closeModal();
-			data.deleteAll(); //清空formData
-			$('.tbody').empty(); //清空列表
-		},
-		error: function (returndata) {
-			alert('failed!')  //可以替换为自己的方法
-		}
+
+async function readfiles(){
+	var dz = document.getElementById('content');
+	var files  = document.getElementById('fileinput').files;
+	const readFileAsync = file => new Promise(resolve => {
+		read = new FileReader();
+			read.readAsBinaryString(file);
+			read.onload = function(){
+				console.log(file.name);
+				console.log(read.result);
+				$('input').remove();
+				var h2e = document.createElement("h2");
+				var pe = document.createElement("p");
+				var bre = document.createElement("br");
+				h2e.innerText = file.name;
+				pe.innerText = read.result;
+				dz.appendChild(h2e);
+				dz.appendChild(pe);
+				dz.appendChild(bre);
+			}	
 	});
+		 	readFileAsync(files[0]);
 }
-// 用事件委托的方法为‘删除’添加点击事件，使用jquery中的on方法
-$(".tbody").on('click','tr td:last-child',function(){
-	//删除拖拽框已有的文件
-	var temp=Dragfiles();
-	var key=$(this).prev().prev().prev().text();
-	console.log(key);
-	temp.delete(key);
-	$(this).parent().remove();
-});
-//清空所有内容
-function clearAll(){
-	if(document.getElementsByTagName('tbody')[0].hasChildNodes()==false){
-		document.getElementById('content').style.borderColor = 'red';
-		setTimeout(blink,300);
-		return false;
-	}
-	var data=Dragfiles(); 
-	data.deleteAll(); //清空formData
-	//$('.tbody').empty(); 等同于以下方法
-	document.getElementsByTagName('tbody')[0].innerHTML='';
-}
+
+
